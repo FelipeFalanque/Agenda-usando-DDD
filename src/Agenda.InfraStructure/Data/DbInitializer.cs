d6f6c0b655d3c5f969708318c8e3df9c6193b060
cesar <cesarvisium@hotmail.com>
2019-04-01 09:03:20 -0300

    DbInitializer Casse que popula o banco de dados

---
+++ src/Agenda.InfraStructure/Data/DbInitializer.cs
@@ -0,0 +1,47 @@
+﻿using Agenda.ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Agenda.InfraStructure.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(Contexto context)
+        {
+            if (context.Contatos.Any())
+            {
+                return;
+            }
+
+            var contatos = new Contato[]
+            {
+                new Contato()
+                {
+                    Nome = "Cesar Oliveira Falanque",
+                    Telefone = "19999996633",
+                },
+                new Contato()
+                {
+                    Nome = "Felipe Oliveira Falanque",
+                    Telefone = "19999996644",
+                },
+                new Contato()
+                {
+                    Nome = "Felipe Falanque",
+                    Telefone = "19999556644",
+                },
+                new Contato()
+                {
+                    Nome = "Oliveira Falanque",
+                    Telefone = "19997796644",
+                }
+            };
+
+            context.Contatos.AddRange(contatos);
+
+            context.SaveChanges();
+        }
+    }
+}
--- src/Agenda.WebSite/Program.cs
+++ src/Agenda.WebSite/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Agenda.InfraStructure.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Agenda.WebSite
@@ -14,7 +16,24 @@ namespace Agenda.WebSite
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var contex = services.GetRequiredService<Contexto>();
+                    DbInitializer.Initialize(contex);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Um erro aconteceu no metodo DbInitializer.Initialize");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
