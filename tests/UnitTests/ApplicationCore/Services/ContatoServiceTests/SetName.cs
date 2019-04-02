5d1903f65f96fc3e67476cd040440bc3111c9222
cesar <cesarvisium@hotmail.com>
2019-04-02 13:11:30 -0300

    alterações

--- tests/UnitTests/ApplicationCore/Services/ContatoServiceTests/SetName.cs
+++ tests/UnitTests/ApplicationCore/Services/ContatoServiceTests/SetName.cs
@@ -2,25 +2,34 @@
 using Agenda.ApplicationCore.Interfaces.Services;
 using Moq;
 using Xunit;
+using System.Linq;
 
 namespace UnitTests.ApplicationCore.Services.ContatoServiceTests
 {
     public class SetName
     {
         
-        //private Mock<IContatoService<ContatoService>> _contatoService;
+        private readonly IContatoService _contatoService;
 
-        public SetName()
+        public SetName(IContatoService contatoService)
         {
-            //_contatoService = new Mock<IContatoService<ContatoService>>();
+            _contatoService = contatoService;
         }
 
         [Fact]
         public void SetandoNomeContato()
         {
-            //var contatos = _contatoService.GetAll();
-
-            Assert.True(true);
+            // Get contato
+            var firstContato = _contatoService.GetAll().FirstOrDefault();
+            // save value in variables
+            var beforeName = firstContato.Nome;
+            var idContato = firstContato.ContatoId;
+            // Set Name
+            firstContato.Nome = "Outro Nome";
+            // save edited contact
+            _contatoService.Update(firstContato);
+            // 
+            Assert.True(!beforeName.Equals(_contatoService.Get(idContato)));
         }
     }
 }
